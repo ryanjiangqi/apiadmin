c2908e1d3d332a42ce611df3a5e63832f1362d14
蒋琦 <jiangqi@51ucar.cn>
2018-11-14 11:20:12 +0800

    ryajiangqi

--- routes/api.php
+++ routes/api.php
@@ -14,22 +14,21 @@ use App\Http\Middleware\OAuthLogin;
 |
 */
 Route::put('login', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken')->middleware(OAuthLogin::class);
-//管理员路由
+//后台路由
 Route::post('user/detail', 'UserController@detail')->middleware('auth:api');
 Route::post('user/update', 'UserController@update')->middleware('auth:api');
 Route::post('user/deleted', 'UserController@deleted')->middleware('auth:api');
 Route::post('user/add/save', 'UserController@addSave')->middleware('auth:api');
-//文章路由
 Route::post('article/add/save', 'ArticleController@addSave')->middleware('auth:api');
 Route::post('article/detail', 'ArticleController@detail')->middleware('auth:api');
 Route::post('article/deleted', 'ArticleController@deleted')->middleware('auth:api');
 Route::post('article/update', 'ArticleController@update')->middleware('auth:api');
-Route::post('article/webdetail', 'ArticleController@webDetail')->middleware('auth:api');
-Route::post('article/productdetail', 'ArticleController@productDetail')->middleware('auth:api');
-
 Route::post('about/detail', 'ArticleController@aboutUs')->middleware('auth:api');
 Route::post('about/update', 'ArticleController@editAbout')->middleware('auth:api');
-
+//前台路由
+Route::post('article/webdetail', 'ArticleController@webDetail');
+Route::post('article/productdetail', 'ArticleController@productDetail');
+Route::post('about/webdetail', 'ArticleController@aboutUs');
 Route::post('upload/image', 'ArticleController@uploadImage');//->middleware('auth:api')
 Route::post('upload/editimage', 'ArticleController@uploadImageEditor');//->middleware('auth:api')
 
