538f476326be9964d2f13698715525c02383de40
蒋琦 <jiangqi@51ucar.cn>
2018-11-16 09:53:48 +0800

    ryan

--- library/helpers.php
+++ library/helpers.php
@@ -4,6 +4,7 @@ function success($data = [], $dataName = 'data', $message = 'success', $status =
 {
     return response()->json(['message' => $message, 'code' => $status, "{$dataName}" => $data], 200);
 }
+
 function error($msg = '未知错误', $code = 0, $httpStatus = 422)
 {
     return response()->json(['message' => $msg, 'code' => $code, 'data' => ''], $httpStatus);
